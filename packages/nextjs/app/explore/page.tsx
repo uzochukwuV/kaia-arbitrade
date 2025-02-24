ee052436a7058ae5347d69791b490fdb0009960f
uzochukwuv <vic.ezealor@gmail.com>
2025-02-24 03:35:11 +0100

    debug vars

--- packages/nextjs/app/explore/page.tsx
+++ packages/nextjs/app/explore/page.tsx
@@ -578,35 +578,6 @@ function MarketNFTState({ id }: { id: string }) {
   );
 }
 
-export function NFTState({ id }: { id: string }) {
-  const [nft, setNFt] = useState<NFTCardProps | null>(null);
-  const { marketplaceData, metadata, isLoading: nftLoading } = useNFTData(id);
-
-  useEffect(() => {
-    setNFt({ ...marketplaceData, ...metadata, id: id });
-  }, [nftLoading, id, marketplaceData]);
-
-  return (
-    <>
-    <NFTCard
-      key={nft?.id}
-      id={nft?.id.toString() || ""}
-      name={nft?.name}
-      description={nft?.description}
-      price={BigInt(nft?.price || 0)}
-      image={nft?.image}
-      owner={nft?.owner}
-      payer={nft?.payer}
-      payedFor={nft?.payedFor}
-      booked={nft?.booked}
-      payerChecked={nft?.payerChecked}
-      buyerChecked={nft?.buyerChecked}
-      quantity={nft?.quantity}
-      harvestDate={nft?.harvestDate}
-    />
-    </>
-  );
-}
 
 export function useNFTData(nftId: string) {
   const { data: tokenUri } = useScaffoldReadContract({
--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { NFTState } from "../explore/page";
+
 import { LoaderIcon } from "react-hot-toast";
 import { useAccount } from "wagmi";
 import { Crop } from "~~/components/ui/Crop";
+import { NFTState } from "~~/components/ui/NFTState";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 import { getParsedError, notification } from "~~/utils/scaffold-eth";
 
